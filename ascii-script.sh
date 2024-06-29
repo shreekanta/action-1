2fa0cce43071d94ac3a0c055110a06b031388f8d
shreekanta <shreekanta.rc@gmail.com>
2024-06-29 21:14:35 +0530

    Executing shell script

--- ascii-script.sh
+++ ascii-script.sh
@@ -3,6 +3,7 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover , Im a Dragon...RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
+
 cowsay -f unicorn "Run for cover , Im a UNICORN...Nehh" >> unicorn.txt
 grep -i "unicorn" unicorn.txt
 cat unicorn.txt
